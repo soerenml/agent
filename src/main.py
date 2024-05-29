82ad35f2fd045992af791d4cd07fa5a110631d64
soeren <soeren.machinelearning@gmail.com>
2024-05-29 22:09:27 +0200

    Simplified model.

--- src/main.py
+++ src/main.py
@@ -2,14 +2,12 @@ import os
 from model import chat_model
 from dotenv import load_dotenv
 
-
+# Reads key value pairs from .env file and adds them to environment variables
 load_dotenv()
-api_key = os.getenv("OPENAI_API_KEY")
 
 out = chat_model(
     system_content='You are a nice chat model',
     user_content='Who invented the wheel?',
-    api_key=api_key
 )
 
 print(out.choices[0].message)
