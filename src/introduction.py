e72c926687ecb6fffd728820a3066f3f695d1212
soeren <soeren.machinelearning@gmail.com>
2024-06-03 22:39:05 +0200

    Finance AI fully working.

--- src/introduction.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Load the environment variables
-load_dotenv()
-
-from langchain_openai import ChatOpenAI
-llm = ChatOpenAI(api_key=os.getenv("OPENAI_API_KEY"))
